1ebb58b6cd9bf07b0f4952051010bf74f7112877
NuGrialeb <110345189+NuGrialeb@users.noreply.github.com>
2023-04-06 19:08:14 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// nav on desktops like viummm slime 
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("heads", window.scrollY > 100);
+});
+
+/// responsive nav
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navlist.classList.remove('open');
+};
+
+var stop = "ERROR ⚠ ERROR ⚠ ERROR ⚠ ERROR ⚠ ERROR ⚠";
+
+console.log(stop);
